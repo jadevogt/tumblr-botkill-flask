dc9c3dd2d5273690b740a76823d88d5cc490f784
jadev-dev <me@jadev.dev>
2023-01-31 21:19:20 -0500

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
-from flask import Flask, session
+from flask import Flask, session, request, redirect
 from os import environ
+import uuid
+from urllib.parse import urlencode
 
 app = Flask(__name__)
 app.secret_key = environ.get("FLASK_SECRET_KEY")
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    """
+    index
+    """
+    return 'main page'
+
+
+@app.route('/auth')
+def auth_handler():
+    """
+    auth
+    """
+    return request.query_string
+
+
+@app.route('/initiate-auth')
+def auth_initiator():
+    """
+    init auth
+    """
+    params = make_url_params()
+    return redirect(f"https://www.tumblr.com/oauth2/authorize?{urlencode(params)}")
+
 
 if __name__ == '__main__':
     app.run()
+
+
+def make_url_params(writeable: bool = False):
+    scope = 'basic write' if writeable else 'basic'
+    state = uuid.uuid4()
+    session['state'] = state
+    params = {
+        'response_type': 'code',
+        'client_id': environ.get('TUMBLR_CONSUMER_KEY'),
+        'redirect_uri': environ.get('REDIRECT_URI'),
+        'scope': scope,
+        'approval_prompt': 'auto',
+        'state': state,
+    }
+    return params
--- tumblr.py
+++ tumblr.py
@@ -1,19 +1,102 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from os import environ
+
+import requests
 
 
-
-@dataclass
+@dataclass(frozen=True)
 class Token:
     """ tumblr auth token """
-    error: str
-    error_description: str
     access_token: str
-    token_type: str
     expires_in: int
+    id_token: bool
     refresh_token: str
     scope: str
+    token_type: str
+    originally_issued: datetime = field(init=False)
+
+    def __post_init__(self):
+        object.__setattr__(self, "originally_issued", datetime.now())
+
+    @property
+    def expired(self) -> bool:
+        """
+        tells whether the token is expired
+        """
+        return datetime.now() - self.originally_issued > timedelta(seconds=self.expires_in)
+
+    def check_scope(self, scope: str) -> bool:
+        """
+        check whether the current token's scope includes the given value
+        """
+        return scope.lower() in self.scope.split(" ")
+
+    @classmethod
+    def from_response(cls, response) -> 'Token':
+        return cls(
+            access_token=response["access_token"],
+            expires_in=response["expires_in"],
+            id_token=response["id_token"],
+            refresh_token=response["refresh_token"],
+            scope=response["scope"],
+            token_type=response["token_type"]
+        )
+
+
+EXAMPLE = {
+    "access_token": "{access_token}",
+    "expires_in": 2520,
+    "id_token": False,
+    "refresh_token": "{refresh_token}",
+    "scope": "write offline_access",
+    "token_type": "bearer"
+}
 
 
 class Tumblr:
+    """
+    Class representing the Tumblr API
+    """
+    consumer_id: str
+    consumer_secret: str
+    token: Token | None
+    redirect_uri: str = environ.get("REDIRECT_URI")
+
+    def __str__(self):
+        return (
+            f"<Tumblr API object | "
+            f"consumer_id: '{self.consumer_id}', "
+            f"token: '{self.token}', "
+            f"redirect_uri: '{self.redirect_uri}'"
+            ">"
+        )
+
+    default_headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': 'PyTumblrBotKill/0.0.1',
+    }
+
     def __init__(self):
-        pass
+        consumer_id = environ.get("TUMBLR_CONSUMER_KEY")
+        consumer_secret = environ.get("TUMBLR_CONSUMER_SECRET")
+        authentication_code = None
+
+    def authenticate(self, authentication_code):
+        body = {
+            'grant_type': 'authorization_code',
+            'client_id': self.consumer_id,
+            'client_secret': self.consumer_secret,
+            'redirect_uri': self.redirect_uri,
+            'code': authentication_code,
+        }
+        response = requests.post(headers=self.default_headers, json=body)
+        self.token = Token.from_response(response.json())
+
+    @property
+    def authenticated(self) -> bool:
+        """
+        checks whether the api object is authenticated
+        """
+        return self.token is not None and not self.token.expired
